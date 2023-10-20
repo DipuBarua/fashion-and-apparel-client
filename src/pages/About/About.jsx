3cd6ae01b9bb8b8fcf8cc9a44273b13ccd74c6e6
DipuBarua <you@example.com>
2023-10-20 20:43:44 +0600

    firebase deployed

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -2,7 +2,7 @@
 const About = () => {
     return (
         <div>
-            <h2 className=" text-3xl font-bold text-center min-h-screen">Lat's know about Fashion and Apparel</h2>
+            <h2 className=" text-3xl font-bold text-center min-h-screen">Lats know about Fashion and Apparel</h2>
             <img src="" alt="" />
         </div>
     );
